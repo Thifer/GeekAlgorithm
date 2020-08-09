6360a50aaa3be725e01a1febbd2a3ec3ae221ff3
Thifer <buybackbrain.genetic@gmail.com>
2020-08-09 10:37:08 +0300

    -complete 14 task

--- lesson1/Program.cs
+++ lesson1/Program.cs
@@ -1,9 +1,52 @@
-﻿namespace GeekAlgorithm
+﻿using System;
+
+namespace GeekAlgorithm
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Введите номер задания или 0 для отмены");
+            int.TryParse(Console.ReadLine(), out var task);
+            Console.WriteLine("task = " + task);
+            switch (task)
+            {
+                case 13:
+                    Console.WriteLine("Введите натуральное число");
+                    int.TryParse(Console.ReadLine(), out var number);
+                    for (int i = 0; i <= number; i++)
+                    {
+                        if (IsAuto(i))
+                        {
+                            Console.Write(i + "^" + 2 +" = " + Math.Pow(i,2)+ " ");
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
+
+        private static bool IsAuto(int i)
+        {
+            if (true)
+            {
+                long pow = (long) Math.Pow(i, 2);
+                pow -= i;
+                for (int j = 0; j < i.ToString().Length; j++)
+                {
+                    if (pow % 10 != 0)
+                        return false;
+                    else
+                    {
+                        pow /= 10;
+                    }
+                }
+                return true;
+            }
+        }
+        
+        
+        
     }
 }
