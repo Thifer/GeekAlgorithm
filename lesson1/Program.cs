e1facb8e99e94104fb0e5f1e74442567a9a535f6
Thifer <buybackbrain.genetic@gmail.com>
2020-08-09 11:09:51 +0300

    -13 task done

--- lesson1/Program.cs
+++ lesson1/Program.cs
@@ -6,11 +6,22 @@ namespace GeekAlgorithm
     {
         public static void Main(string[] args)
         {
+            int seed = 0;
             Console.WriteLine("Введите номер задания или 0 для отмены");
             int.TryParse(Console.ReadLine(), out var task);
             Console.WriteLine("task = " + task);
             switch (task)
             {
+                case 13:
+                    Random rnd = new Random();
+
+                    for (int i = 0; i < 10; i++)
+                    {
+                        Console.WriteLine("Случайное число с использованием стандартного ГПСЧ : " + rnd.Next(1, 100));
+                        Console.WriteLine("Случайное число без использования стандартного ГПСЧ : " + Rnd(ref seed));
+                    }
+                    
+                    break;
                 case 14:
                     Console.WriteLine("Введите натуральное число");
                     int.TryParse(Console.ReadLine(), out var number);
@@ -18,9 +29,10 @@ namespace GeekAlgorithm
                     {
                         if (IsAuto(i))
                         {
-                            Console.Write(i + "^" + 2 +" = " + Math.Pow(i,2)+ " ");
+                            Console.Write(i + "^" + 2 + " = " + Math.Pow(i, 2) + " ");
                         }
                     }
+
                     break;
                 default:
                     break;
@@ -42,11 +54,24 @@ namespace GeekAlgorithm
                         pow /= 10;
                     }
                 }
+
                 return true;
             }
         }
-        
-        
-        
+
+        private static int Rnd(ref int seed)
+        {
+            seed = seed + DateTime.Now.GetHashCode();
+            int result = seed;
+            result >>= 10;
+            result /= 1243;
+            while (result < 0 || result > 100)
+            {
+                if (result <= 0) result += 100;
+                else if (result >= 100) result -= 100;
+            }
+
+            return result;
+        }
     }
 }
