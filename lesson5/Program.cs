52fdc3c3a477b4552648183ec2d3ea198f074e4c
Thifer <buybackbrain.genetic@gmail.com>
2020-08-23 12:37:00 +0300

    done lesson5-2

--- lesson5/Program.cs
+++ lesson5/Program.cs
@@ -1,24 +1,28 @@
 ﻿using System;
 using System.Collections;
+using System.Globalization;
 
-namespace lesson4_5
+namespace lesson5
 {
     //
     // Написать программу, которая определяет, является ли введенная скобочная последовательность правильной. Примеры правильных скобочных выражений: (), ([])(), {}(), ([{}]), неправильных — )(, ())({), (, ])}), ([(]) для скобок [,(,{. Например: (2+(2*2)) или [2/{5*(4+7)}]
     // Реализовать алгоритм перевода из инфиксной записи арифметического выражения в постфиксную.
     // 
+
+
     internal class Program
     {
+        static int[,] Field;
+
         public static void Main(string[] args)
         {
-            Check();
+            //Check();
+            transfer(Console.ReadLine());
         }
-
 
 
         private static void Check()
         {
-            
             Stack stack = new Stack();
 
             Console.WriteLine("ВВедите выражение");
@@ -31,9 +35,16 @@ namespace lesson4_5
                     stack.Push(a);
                 }
 
+
                 if (a == ')' || a == '}' || a == ']')
                 {
-                    if (checks((char)stack.Peek(),a))
+                    if (stack.Count == 0)
+                    {
+                        Console.WriteLine("не верно");
+                        return;
+                    }
+
+                    if (checks((char) stack.Peek(), a))
                     {
                         stack.Pop();
                     }
@@ -46,7 +57,14 @@ namespace lesson4_5
                 }
             }
 
-            Console.WriteLine("верно");
+            if (stack.Count > 0)
+            {
+                Console.WriteLine("не верно");
+            }
+            else
+            {
+                Console.WriteLine("верно");
+            }
         }
 
 
@@ -58,6 +76,34 @@ namespace lesson4_5
             }
 
             return false;
+        }
+
+
+        private static void transfer(string a)
+        {
+            Stack op = new Stack();
+            string answer =string.Empty;
+            foreach (var ch in a)
+            {
+
+                if (ch == '1' || ch == '2' || ch == '3' || ch == '4'|| ch == '5' || ch == '6' || ch == '7' || ch == '8' ||
+                    ch == '9')
+                {
+                    answer += ch;
+                }
+                if (ch == '+' || ch == '-'|| ch == '*' || ch == '/')
+                {
+                    op.Push(ch);
+                }
+
+            }
+
+            while (op.Count>0)
+            {
+                answer += op.Pop();
+            }
+
+            Console.WriteLine(answer);
         }
     }
 }
